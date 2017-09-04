aa64ba4e9d705898ffe0d97fc3cd0d32b28550f0
Hans <hans.lienhop@gmail.com>
2017-09-04 20:16:53 +0200

    started pie

---
+++ sidebar.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("Sidenav").style.width = "280px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNavRight() {
+    document.getElementById("Sidenav").style.width = "0";
+}

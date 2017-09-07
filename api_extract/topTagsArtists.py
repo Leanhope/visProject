4d3346b4c9e85d34c2e9350a7b02c7c643224062
der-freddy <frederik.sukop@uni-weimar.de>
2017-09-07 16:51:34 +0200

    comment index.html

--- api_extract/topTagsArtists.py
+++ api_extract/topTagsArtists.py
@@ -6,15 +6,16 @@ import requests
 import os
 import glob
 
+#get for all top 5 artists per country the top 10 tags. This helps to visualize the bubble chart
+
 url = 'http://ws.audioscrobbler.com/2.0/?method=artist.gettoptags&api_key=ba3772fe9087004cadb3715a2f170555&format=json' #geoArtists
 patha = "geoTopArtists/*.json"
 patht = "artistTopTags/"
 final = str()
 
 
-
+#get the top artist files per country
 for f in glob.glob(patha):
-	
 	final += "id,value\ntag,\n"
 	
 	country = f.split('/')
@@ -25,20 +26,21 @@ for f in glob.glob(patha):
 	countrylist = countrylist.read()
 	countrylist = json.loads(countrylist)
 
+	#get countries and codes for linking
 	for i in countrylist:
 		code = i["Code"]
 		name = i["Name"]
 
+		#create .csv file for saving
 		if country == name:
-
 			w = open("topTagsArtistsCountries/"+code+".csv", 'w')
-			print()
+			
 
 	with open(f, 'r') as handle:
 		parsed = json.load(handle)
 		print(f)
-
 	
+	#get top 5 tags from artistTopTag.json
 	for i in range(0, 5):
 		try:
 			artist = parsed["topartists"]["artist"][i]["name"]
@@ -48,6 +50,7 @@ for f in glob.glob(patha):
 			tagfile = tagfile.read()
 			tagfile = json.loads(tagfile)
 
+			#create sting
 			for i in range(0, 10):
 				tag = tagfile["toptags"]["tag"][i]["name"]
 				count = tagfile["toptags"]["tag"][i]["count"]
@@ -55,13 +58,13 @@ for f in glob.glob(patha):
 	
 		except Exception as e:
 			print(e)
+	#try to write if file exists
 	try:
-
 		w.write(final)
 		final = str()			
 		w.close()	
 	except Exception as e:
-			print(e)
-			w.close()
+		print(e)
+		w.close()
 
 
